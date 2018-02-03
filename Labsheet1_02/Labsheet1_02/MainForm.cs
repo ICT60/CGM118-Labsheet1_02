edb4c01065e3d6d2e9a717ca3af0b1ca79bba880
Chatchai Saratakij <CSaratakij@gmail.com>
2018-02-03 21:47:55 +0700

    Added ui to edit and show a square's area.

--- Labsheet1_02/Labsheet1_02/MainForm.cs
+++ Labsheet1_02/Labsheet1_02/MainForm.cs
@@ -1,20 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Labsheet1_02
 {
     public partial class MainForm : Form
     {
+        Square square;
+
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        void MainForm_Load(object sender, EventArgs e)
+        {
+            _Initialize();
+        }
+
+        void btnArea_Click(object sender, EventArgs e)
+        {
+            _ShowSquareAreaHandler();
+        }
+
+        void _Initialize()
+        {
+            square = new Square();
+            txtWidth.Text = square.Width.ToString();
+            txtHeight.Text = square.Height.ToString();
+        }
+
+        void _ShowSquareAreaHandler()
+        {
+            double width = 0.0d;
+            double height = 0.0d;
+
+            var isCanParse = 
+                double.TryParse(txtWidth.Text, out width) &&
+                double.TryParse(txtHeight.Text, out height);
+
+            if (isCanParse)
+            {
+                square.Width = width;
+                square.Height = height;
+
+                var result = string.Format("Area : {0}", square.Area);
+                MessageBox.Show(result, "Result");
+            }
+            else
+            {
+                MessageBox.Show("Please enter only number..");
+            }
         }
     }
 }
